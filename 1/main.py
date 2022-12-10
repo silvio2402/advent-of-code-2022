e2b99a1ac11eccc70e8d55aaf8f13ea46940c12d
Silvio Brändle <silvio@silviobraendle.ch>
2022-12-10 12:38:51 +0100

    Format & Return part 1

--- 1/main.py
+++ 1/main.py
@@ -2,19 +2,20 @@ import os
 
 
 def main():
-  with open(os.path.join(os.path.dirname(__file__), "./input.txt"), 'r') as f:
-    lines = f.readlines()
-    current_calories = 0
-    calories_list = []
-    for line in lines:
-      line = line.strip()
-      if line != "":
-        current_calories += int(line)
-      else:
-        calories_list.append(current_calories)
+    with open(os.path.join(os.path.dirname(__file__), "./input.txt"), 'r') as f:
+        lines = f.readlines()
         current_calories = 0
-  calories_topthree = sorted(calories_list, reverse=True)[:3]
-  return sum(calories_topthree)
+        calories_list = []
+        for line in lines:
+            line = line.strip()
+            if line != "":
+                current_calories += int(line)
+            else:
+                calories_list.append(current_calories)
+                current_calories = 0
+    calories_topthree = sorted(calories_list, reverse=True)[:3]
+    return max(calories_list), sum(calories_topthree)
+
 
 if __name__ == "__main__":
-  print(main())
+    print(main())
