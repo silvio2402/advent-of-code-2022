ff590a46899cc170054d7c32c7c587879f75a537
Silvio Brändle <silvio@silviobraendle.ch>
2022-12-10 15:35:21 +0100

    Solved day 2

---
+++ 2/main.py
@@ -0,0 +1,52 @@
+import os
+
+
+def main():
+    with open(os.path.join(os.path.dirname(__file__), "./input.txt"), 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        total_score_a = 0
+        for line in lines:
+            opponent, you = line.split(" ")
+            opponent = ord(opponent) - ord('A')
+            you = ord(you) - ord('X')
+            score = 1 + you
+            if opponent == you:
+                # Draw
+                score += 3
+            elif opponent == (you + 1) % 3:
+                # Lose
+                score += 0
+            else:
+                # Win
+                score += 6
+            total_score_a += score
+
+        total_score_b = 0
+        for line in lines:
+            opponent, end = line.split(" ")
+            opponent = ord(opponent) - ord('A')
+            score = 0
+            if end == "X":
+                # Lose
+                you = (opponent + 2) % 3
+                score += 1 + you
+            elif end == "Y":
+                # Draw
+                score += 3
+                you = opponent
+                score += 1 + you
+            else:
+                # Win
+                score += 6
+                you = (opponent + 1) % 3
+                score += 1 + you
+            total_score_b += score
+
+    return total_score_a, total_score_b
+
+
+if __name__ == "__main__":
+    a, b = main()
+    print(a)
+    print(b)
